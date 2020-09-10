356dde9f73b7866e6c950e8284ed2530f112ab7d
Oscar Torres <torres9312@hotmail.com>
2020-09-09 19:07:28 -0500

    add parallax

--- js/app.js
+++ js/app.js
@@ -1,33 +1,63 @@
 var cont;
+var imgcont;
 var LIMIT = 3;
 var MIN = 1;
 
 window.onload = function() {
     $('#video1').show();
     cont = 1;
+    $('#img1').show();
+    imgcont = 1;
   };
 
 function advance(e){
     $("#player"+cont).trigger('pause');
     if(cont == LIMIT){
         $("#video"+cont).hide();
-        $("#video1").fadeIn(500);
+        $("#video"+MIN).show();
         cont = 1;
     }else{
         $("#video"+cont).hide();
         cont++;
-        $("#video"+cont).fadeIn(500);  
+        $("#video"+cont).show();  
     }
 }
+
 function preview(e){
     $("#player"+cont).trigger('pause');
     if(cont == 1){
         $("#video"+cont).hide();
-        $("#video"+LIMIT).fadeIn(500);
+        $("#video"+LIMIT).show();
         cont = LIMIT;
     }else{
         $("#video"+cont).hide();
         cont--;
-        $("#video"+cont).fadeIn(500);  
+        $("#video"+cont).show();  
+    }
+}
+
+
+function next(e){
+
+    if(imgcont == LIMIT){
+        $("#img"+imgcont).hide();
+        $("#img"+MIN).show();
+        imgcont = 1;
+    }else{
+        $("#img"+imgcont).hide();
+        imgcont++;
+        $("#img"+imgcont).show();  
+    }
+}
+function prev(e){
+    
+    if(imgcont == 1){
+        $("#img"+imgcont).hide();
+        $("#img"+LIMIT).show();
+        imgcont = LIMIT;
+    }else{
+        $("#img"+imgcont).hide();
+        imgcont--;
+        $("#img"+imgcont).show();  
     }
 }
