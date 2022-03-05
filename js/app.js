b0fa5e82bc018a831c3bc6cf964088bc1ffe21b1
Oscar Torres <torres9312@hotmail.com>
2022-03-04 21:22:15 -0600

    update web site

--- js/app.js
+++ js/app.js
@@ -1,93 +1,261 @@
-var cont;
-var imgcont;
-var cont3;
-var LIMIT = 3;
-var MIN = 1;
+let mountain = document.getElementById('mountain');
+let men = document.getElementById('men');
+let text = document.getElementById('text');
+let cover = document.getElementById('cover');
+let sheet = document.getElementById('sheet');
+/* mountain.style.transform = 'scale(1.03)'; */
 
-var LIMIT3 = 6;
+var scale = 1;
 
-window.onload = function() {
-    $('#video1').show();
-    cont = 1;
-    $('#img1').show();
-    imgcont = 1;
-    $('#imgLaravel1').show();
-    cont3 = 1;
-  };
-
-function advance(e){
-    $("#player"+cont).trigger('pause');
-    if(cont == LIMIT){
-        $("#video"+cont).hide();
-        $("#video"+MIN).show();
-        cont = 1;
-    }else{
-        $("#video"+cont).hide();
-        cont++;
-        $("#video"+cont).show();  
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      /* toast.addEventListener('mouseenter', Swal.stopTimer) */
+      /* toast.addEventListener('mouseleave', Swal.resumeTimer) */
     }
-}
-
-function preview(e){
-    $("#player"+cont).trigger('pause');
-    if(cont == 1){
-        $("#video"+cont).hide();
-        $("#video"+LIMIT).show();
-        cont = LIMIT;
-    }else{
-        $("#video"+cont).hide();
-        cont--;
-        $("#video"+cont).show();  
-    }
-}
+  })
 
 
-function next(e){
+  function scrollToElement(section, e){
+      e.preventDefault();
+      /* document.getElementById(section).scrollIntoView(); */
 
-    if(imgcont == LIMIT){
-        $("#img"+imgcont).hide();
-        $("#img"+MIN).show();
-        imgcont = 1;
-    }else{
-        $("#img"+imgcont).hide();
-        imgcont++;
-        $("#img"+imgcont).show();  
-    }
-}
-function prev(e){
+      if(!cover.classList.contains('hide')){
+          cover.classList.add('hide');
+      }
+
+      switch (section) {
+          case '1':
+            window.scrollTo(0, 809);
+              break;
+          case '2':
+            window.scrollTo(0, 1366);
+              break;
+          case '3':
+            window.scrollTo(0, 1894);
+              break;
+          case '4':
+            window.scrollTo(0, 2697);
+              break;
+          case '5':
+            window.scrollTo(0, 3369);
+              break;
+      
+          default:
+              break;
+      }
+      
+  }
+
+
+$(document).ready(function(){
+    const elText = $('#text-animation');
+    const eltext2 = $('#text-animation');
+
+    const text = "Hola Mundo";
+
+    /* CHARTS SKILLS */
+    $('#php').css('width', '70%');
+    $('#javascript').css('width', '90%');
+    $('#cmas').css('width', '50%');
+    $('#java').css('width', '70%');
+    $('#html').css('width', '100%');
+    $('#css').css('width', '90%');
+    $('#laravel').css('width', '50%');
+    $('#angular').css('width', '80%');
+    $('#node').css('width', '30%');
+    $('#mysql').css('width', '80%');
+    $('#postgre').css('width', '50%');
+    $('#typescript').css('width', '60%');
+    $('#ionic').css('width', '40%');
     
-    if(imgcont == 1){
-        $("#img"+imgcont).hide();
-        $("#img"+LIMIT).show();
-        imgcont = LIMIT;
-    }else{
-        $("#img"+imgcont).hide();
-        imgcont--;
-        $("#img"+imgcont).show();  
-    }
-}
 
-function next3(e){
+    let idx = 1; 
+    let speed = 300 / 2;
 
-    if(cont3 == LIMIT3){
-        $("#imgLaravel"+cont3).hide();
-        $("#imgLaravel"+MIN).show();
-        cont3 = 1;
-    }else{
-        $("#imgLaravel"+cont3).hide();
-        cont3++;
-        $("#imgLaravel"+cont3).show();  
+    writeText();
+
+
+    function writeText(){
+        elText.html(text.slice(0,idx)+"<span class='TypeCursor'>&nbsp;</span>");
+        /* eltext2.html(text2.slice(0,idx)+"<span class='TypeCursor'>&nbsp;</span>"); */
+
+        idx++;
+
+        if(idx > text.length){
+            /* console.log($('#text-animation').html()); */
+            setTimeout(function(){
+                idx = 1;
+                setTimeout(writeText,speed);
+            }, 3000);
+            
+            
+        }else{
+            setTimeout(writeText,speed);
+        }
+
+        
     }
-}
-function prev3(e){
+
     
-    if(cont3 == 1){
-        $("#imgLaravel"+cont3).hide();
-        $("#imgLaravel"+LIMIT3).show();
-        cont3 = LIMIT3;
+
+            var events = [{
+                    date: '2015 - 2019',
+                    content: 'Universidad Veracruzana <small>Ingeniería de Software</small><i class="fa fa-graduation-cap icon-timeline"></i>'
+                },
+                {
+                    date: 'Ene 2020 - Jul 2020',
+                    content: 'Facultad de Estadística e Informática <small> Desarrollador en Java </small><img class="img-timeline" src="src/img/java.png" alt="java">'
+                },
+                {
+                    date: 'Agost 2020 - Dic 2020',
+                    content: 'Instituto Educativo Panamericano <small>Desarrollador PHP</small><img class="img-timeline" src="src/img/php.png" alt="php">'
+                },
+                {
+                    date: 'Agost 2020 - Dic 2020',
+                    content: 'Americleann - USA <small>Desarrollador Web</small><img class="img-timeline" src="src/img/html.png" alt="javascript">'
+                },
+                
+                {
+                    date: 'Ene 2021 - Agost 2021',
+                    content: 'Fidelicoin <small>Desarrollador Angular & Laravel</small><img class="img-timeline" src="src/img/angular.png" alt="javascript">'
+                },
+                {
+                    date: 'Sept 2021 - Dic 2021',
+                    content: 'SEDARPA Veracruz <small>Desarrollador Laravel</small><img class="img-timeline" src="src/img/laravel.png" alt="javascript">'
+                },
+                {
+                    date: 'Ene 2021 - Ene 2022',
+                    content: 'Gobierno del Estado De Veracruz <small>Angular & Java EE</small><img class="img-timeline" src="src/img/angular.png" alt="javascript">'
+                }
+            ];
+
+            $('#experience-timeline').roadmap(events, {
+                eventsPerSlide: 4,
+                slide: 1,
+                prevArrow: '<i class="fa fa-chevron-left"></i>',
+                nextArrow: '<i class="fa fa-chevron-right"></i>'
+            });
+            
+
+   
+});
+
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+    /* console.log(value); */
+    mountain.style.width = 100 - value /7 + '%'; 
+    men.style.width = 100 - value /7 + '%'; 
+    /* men.style.right = value /20 + '%'; 
+    mountain.style.left = value /20 + '%';  */
+    text.style.top = value * .10 + '%';
+    sheet.style.top = value * .10 + '%';
+    text.style.opacity = (180 - value) / 100;
+
+    if(value > 465){
+        cover.classList.add('hide');
     }else{
-        $("#imgLaravel"+cont3).hide();
-        cont3--;
-        $("#imgLaravel"+cont3).show();  
+        cover.classList.remove('hide');
     }
+})
+
+$('#form-mail').on('submit', function(e){
+    e.preventDefault();
+    var form = $(this).serializeArray();
+    var formClean = [];
+    const formData = new FormData();
+
+      
+      for (let index = 0; index < form.length; index++) {
+            const element = form[index];
+            element.value = element.value.trim();
+
+            /* console.log(element.value.length); */
+          if(element.name != 'company' ){
+            if(element.value === ''   || element.value.length === 0){
+                Toast.fire({
+                    icon: 'warning',
+                    title: 'No puedes enviar caracteres vacios en tu mensaje.'
+                })
+                /* $('#sendemail').css('pointer-events', 'auto'); */
+                return;
+
+            }
+
+            if(element.value.length > 400){
+                Toast.fire({
+                    icon: 'warning',
+                    title: 'Parece que tu mensaje es muy largo, intenta con otro mensaje.'
+                })
+                /* $('#sendemail').css('pointer-events', 'auto');   */
+                return;
+            }
+
+          }
+
+          formData.append(element.name,element.value);
+          
+      }
+        console.log(formData.get('message'));
+
+
+        sendContactData(formData);
+      
+      
+})
+
+
+
+
+
+function clearformEmail(){
+    $('#form-mail #name').val("");
+    $('#form-mail #company').val("");
+    $('#form-mail #email').val("");
+    $('#form-mail #location').val("");
+    $('#form-mail #message').val("");
 }
+
+
+function sendContactData(data){
+    $.ajax({
+        url: 'functions/send_email.php',
+        type: 'POST',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        data: data,
+        beforeSend : function (){
+            $('#sendemail').html('Enviando...');
+            $('#sendemail').css('pointer-events', 'none');  
+        }
+    }).done(function(res){
+        $('#sendemail').html('Enviar');  
+        $('#sendemail').css('pointer-events', 'auto'); //RESTAURAR EVENTOS DEL BOTON
+        
+        Toast.fire({
+            icon: 'success',
+            title: 'Mensaje enviado,<br> me pondré en contacto con usted.'
+        })
+        clearformEmail();
+        
+        console.log("Mensaje:"+ res.message);
+
+    }).always(function(){
+
+    }).fail(function(res,status,error){
+        $('#sendemail').html('Enviar');   
+        $('#sendemail').css('pointer-events', 'auto'); //RESTAURAR EVENTOS DEL BOTON
+        Toast.fire({
+            icon: 'error',
+            title: 'Error al enviar el mensaje'
+        })
+        console.log(res);
+    });
+}
+
+
